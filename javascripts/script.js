a7fa92590f1fe6e332881c3f6932732a0c5e9603
Mike Ulvila <mikeulvila@me.com>
2016-04-14 15:57:11 -0500

    adding color background with function return iterating over myStyles array

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,6 +1,18 @@
-d3.selectAll('.item:nth-child(3)')
-  .classed({
-    'highlight': true,
-    'item': false,
-    'bigger': true
-  })
+var myStyles = [
+  '#A57706',
+  '#BD3613',
+  '#D11C24',
+  '#C61C6F',
+  '#595AB7',
+  '#2176C7'
+]
+
+
+d3.selectAll('.item')
+  .data(myStyles)
+  .style({
+    'color': 'white',
+    'background': function (d) {
+      return d;
+    }
+  });
