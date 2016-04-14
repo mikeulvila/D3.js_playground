15a1764bc76135bd17c3e655618d314b6c26e60b
Mike Ulvila <mikeulvila@me.com>
2016-04-14 16:21:37 -0500

    adding names dynamically through D3 object

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,21 +1,32 @@
 var myStyles = [
   { width: 200,
+    name: 'Mike',
     color: '#A57706'},
   { width: 230,
+    name: 'Alli',
     color: '#BD3613'},
   { width: 220,
+    name: 'Jackson',
     color: '#D11C24'},
   { width: 290,
+    name: 'Brynn',
     color: '#C61C6F'},
   { width: 236,
+    name: 'Knox',
     color: '#595AB7'},
   { width: 230,
+    name: 'Emmett',
     color: '#2176C7'}
 ]
 
 
-d3.selectAll('.item')
+d3.selectAll('#chart').selectAll('div')
   .data(myStyles)
+  .enter().append('div')
+  .classed('item', true)
+  .text(function (d) {
+     return d.name;
+  })
   .style({
     'color': 'white',
     'background': function (d) {
