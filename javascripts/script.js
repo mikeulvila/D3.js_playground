62cb4802e2cabf995217dd1c0a0fbe60ba66fd85
Mike Ulvila <mikeulvila@me.com>
2016-04-15 07:00:50 -0500

    using d3.scale.linear to scale chart to fit div no matter how much data is put in

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,9 +1,13 @@
-var bardata = [20, 30, 45, 15, 50];
+var bardata = [20, 200, 300, 30, 45, 15, 50, 20, 30, 45, 15, 50];
 
 var height = 400,
     width = 600,
     barWidth = 50,
     barOffset = 5;
+
+var yScale = d3.scale.linear()
+             .domain([0, d3.max(bardata)])
+             .range([0, height])
 
 d3.select('#chart').append('svg')
   .attr('width', width)
@@ -14,11 +18,11 @@ d3.select('#chart').append('svg')
     .style('fill', '#C61C6F')
     .attr('width', barWidth)
     .attr('height', function (data) {
-       return data;
+       return yScale(data);
     })
     .attr('x', function (data, index) {
        return index * (barWidth + barOffset);
     })
     .attr('y', function (data) {
-       return height - data;
+       return height - yScale(data);
     })
